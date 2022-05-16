e5830e6423da8126d7297439226be5d536b0007d
Eugene Morozenko <morozenko-2014@mial.ru>
2022-05-16 20:09:49 +0300

    Font replacement
    Changes in styles at the request of the customer

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,8 @@
-const hamburger = document.querySelector('.hamburger'),
+const hamburger = document.querySelector('.menu__btn'),
       menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger__active');
     menu.classList.toggle('menu__active');
 });
+
