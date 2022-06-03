751adff4650c724d0c30f1c7887d74ce9ac12fc7
Eugene Morozenko <morozenko-2014@mial.ru>
2022-06-03 07:57:17 +0300

    Card
    Card page for sofa

---
+++ src/js/gallery.js
@@ -0,0 +1,8 @@
+let gallery = document.querySelector('.gallery__main-img'),
+    galleryItem = document.querySelector('.gallery__item-min'),
+    translateWidth = gallery.offsetWidth,
+    position = 0;
+
+
+
+gallery.style.transform = `translateX(${position}px)`
