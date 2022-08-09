af7d804e5d13f8c9842f4613816d4b2f064b8f87
Eugene Morozenko <72557729+Front-Eugene@users.noreply.github.com>
2022-08-09 07:56:14 +0300

    Gallery slider.
    I guess it's last commit. This project already has hosted and you can see last version here https://soff.by

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,8 +1,62 @@
-let screen = document.querySelector('.gallery__screen');
-    img = document.querySelectorAll('.gallery__item');
+let screen = document.querySelector('.gallery__screen'),
+    img = document.querySelectorAll('.gallery__item'),
+    gallerySlider = document.querySelector('.gallery__slider'),
+    btnClose = document.querySelector('.btn__close'),
+
+
+    images = document.querySelectorAll('.gallery__slider .slider__line img'),
+    sliderLine = document.querySelector('.slider__line'),
+    btnPrev = document.querySelector('.btn__prev'),
+    btnNext = document.querySelector('.btn__next'),
+    count = 0,
+    width;
 
     img.forEach(item => {
         item.addEventListener('click', function() {
             screen.innerHTML = '<img' +  item.getAttribute('data-link') + '>';
         })
     });
+
+    screen.addEventListener('click', function() {
+        gallerySlider.classList.toggle('active')
+        document.body.style.overflow = 'hidden'
+    })
+
+    btnClose.addEventListener('click', function() {
+        gallerySlider.classList.toggle('active')
+        document.body.style.overflow = ''
+    })
+
+
+
+    function init() {
+        width = document.querySelector('.gallery__slider').offsetWidth;
+        sliderLine.style.width = width * images.length + 'px';
+        images.forEach(item => {
+            item.style.width = width + 'px';
+        })
+        scrollSlider();
+    }
+
+    window.addEventListener('resize', init);
+    init();
+
+    btnNext.addEventListener('click', function() {
+        count++;
+        if(count >= images.length) {
+            count = 0;
+        }
+        scrollSlider();
+    })
+
+    btnPrev.addEventListener('click', function() {
+        count--;
+        if(count < 0) {
+            count = images.length - 1;
+        }
+        scrollSlider();
+    })
+
+    function scrollSlider() {
+        sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+    }
